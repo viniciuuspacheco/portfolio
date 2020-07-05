00f97697b5229f0b5082d50bc56df38726188dd1
PACHECO <pachecoelitesoft@gmail.com>
2020-07-05 03:42:06 -0300

    Profile finalizado

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
+  data = new Date();
+  idade = 0;
 
 
 
@@ -14,6 +16,18 @@ export class ProfileComponent implements OnInit {
   }
 
   ngOnInit(): void {
-  }
+    let dia = this.data.getDate();
+    let mes = this.data.getMonth() + 1;
+    let ano = this.data.getFullYear();
 
+    if (mes > 7) {
+      this.idade = ano - 1992;
+    }
+    if (mes === 7 && dia >= 9) {
+      this.idade = ano - 1992;
+    }
+    else {
+      this.idade = (ano - 1992) - 1;
+    }
+  }
 }
